ad31fb8aa89bbd833a1e5bcc6aeb0c99131cc2d5
nyseer1 <nyseer.couse@gmail.com>
2026-07-03 14:32:09 -0400

    attempt to fix turbopack root folder error

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: process.cwd(), // Ensures it uses the current project root
+  }
 };
 
 export default nextConfig;
