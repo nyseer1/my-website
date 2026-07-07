453024989960ca0eaa50c43dc1e67d6f0aa7673b
nyseer1 <nyseer.couse@gmail.com>
2026-07-07 02:46:59 -0400

    improvements to navbar, and responsiveness

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,21 @@
 import Link from "next/link"; //<a> but allows for prefetching of specified links
 import Hamburger from "./Hamburger";
 export default function Navbar() {
+
+	//hide on screen drag (optional)
+		// var prevScrollpos = window.pageYOffset;
+	// window.onscroll = () => {
+	// 	var currentScrollPos = window.pageYOffset;
+	// 	if (prevScrollpos > currentScrollPos) {
+	// 		document.getElementById("navbar").style.top = "0";
+	// 	} else {
+	// 		document.getElementById("navbar").style.top = "-50px";
+	// 	}
+	// 	prevScrollpos = currentScrollPos;
+	// };
+
 	return (
-		<ul>
+		<ul id="navbar">
 			<li>
 				<Link href="/" style={{ backgroundColor: "black" }}>
 					Nyseer
@@ -12,16 +25,16 @@ export default function Navbar() {
 				<Link href="/">Home</Link>
 			</li>
 			<li className="navbarItems">
-				<Link href="#about">About Me</Link>
+				<Link href="/#about">About Me</Link>
 			</li>
 			<li className="navbarItems">
-				<Link href="#projects">Projects</Link>
+				<Link href="/#projects">Projects</Link>
 			</li>
 			<li className="navbarItems">
-				<Link href="#contact">Contact Me</Link>
+				<Link href="/#contact">Contact Me</Link>
 			</li>
 			<li className="icon">
-				<Hamburger/>
+				<Hamburger />
 			</li>
 		</ul>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
+'use client'
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 export default function HomePage() {
+	const [isDesktop, setDesktop] = useState(null);
+
+	const updateMedia = () => {
+		setDesktop(window.innerWidth > 600);
+	};
+
+	useEffect(() => {
+		window.addEventListener("resize", updateMedia);
+		return () => window.removeEventListener("resize", updateMedia);
+	});
 	return (
 		<>
 			<div id="home">
 				<Navbar />
 				{/* test */}
+				{isDesktop ? (
+					<br style={{ lineHeight: 3 }} />
+				) : (
+					<br style={{ lineHeight: 6 }} />
+				)}
 				<h3>Hi, Im Nyseer Couse, Welcome to my Website!</h3>
 				<br style={{ lineHeight: 10 }} />
 			</div>
@@ -27,10 +44,16 @@ export default function HomePage() {
 				{/* test */}
 				<h1>Contact</h1>
 				<p id="contact-p">
-					<b>Phone:</b> <a href="tel:+1-347-579-9610">(347)-579-9610</a><br/>
-					<b>Email:</b> <a href="mailto:nyseer.couse@gmail.com" aria-label="nyseer.couse@gmail.com"
-         >nyseer.couse@gmail.com</a>
-					<br/>
+					<b>Phone:</b> <a href="tel:+1-347-579-9610">(347)-579-9610</a>
+					<br />
+					<b>Email:</b>{" "}
+					<a
+						href="mailto:nyseer.couse@gmail.com"
+						aria-label="nyseer.couse@gmail.com"
+					>
+						nyseer.couse@gmail.com
+					</a>
+					<br />
 				</p>
 				<br style={{ lineHeight: 10 }} />
 			</div>
